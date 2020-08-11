ca620c345bfe481bbfcb083cd4a3b14ed2955101
CodyNZ <61666737+CodyNZ@users.noreply.github.com>
2020-08-11 16:55:05 +1200

    Match on and off

---
+++ main.js
@@ -0,0 +1,3 @@
+<button onclick="document.getElementById('myImage').src='Match_lit.jpg'">Turn on the light</button>
+<img id="myImage" src="Match_unlit.jpg" style="width:100px">
+<button onclick="document.getElementById('myImage').src='Match_unlit.jpg'">Turn off the light</button>
